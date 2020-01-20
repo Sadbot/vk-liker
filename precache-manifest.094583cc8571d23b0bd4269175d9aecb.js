7e07a4edb5d1fc589f5ec68b17d83905effe5c67
semen emelianov <semen.emelianov@simbirsoft.com>
2020-01-20 16:19:54 +0400

    Updates

---
+++ precache-manifest.094583cc8571d23b0bd4269175d9aecb.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ed7d68295cd5acdefbd56efcda5d36e6",
+    "url": "/vk-liker/index.html"
+  },
+  {
+    "revision": "91b7d4af6e2e8987fb22",
+    "url": "/vk-liker/static/css/2.2cfa1b23.chunk.css"
+  },
+  {
+    "revision": "c366700241eb182040e6",
+    "url": "/vk-liker/static/css/main.981f7211.chunk.css"
+  },
+  {
+    "revision": "91b7d4af6e2e8987fb22",
+    "url": "/vk-liker/static/js/2.51eb1c9e.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/vk-liker/static/js/2.51eb1c9e.chunk.js.LICENSE"
+  },
+  {
+    "revision": "c366700241eb182040e6",
+    "url": "/vk-liker/static/js/main.ea14fb96.chunk.js"
+  },
+  {
+    "revision": "f2ac1995085c7a4f4c83",
+    "url": "/vk-liker/static/js/runtime-main.b14dc6b5.js"
+  },
+  {
+    "revision": "4e1ec8403d903dc514271d7328fbdeb3",
+    "url": "/vk-liker/static/media/persik.4e1ec840.png"
+  }
+]);
